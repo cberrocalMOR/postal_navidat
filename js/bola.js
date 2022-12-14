1cd48b666ba725eceff14699fba660ef2445bcb1
artalcb <artalcb@gmail.com>
2022-12-14 11:59:55 +0100

    snow update

--- js/bola.js
+++ js/bola.js
@@ -6,11 +6,15 @@ for (var i = 0; i < SNOW_BALLS; i++) {
 }
 
 function generateBalls(var1){
+
+    var randomDeep = Math.random();
     var letItSnow = document.createElement("div");
     var random = (Math.random() * 160)-30;
     letItSnow.classList.add("bola");
     letItSnow.style.left = random+"%";
-    letItSnow.style.zIndex = Math.floor((Math.random() * 2)-1);
+
+    letItSnow.style.filter = "opacity("+randomDeep+")";
+    letItSnow.style.zIndex = Math.floor((randomDeep * 2)-1.5);
     letItSnow.style.animationDelay = var1+"s, "+(var1+Math.random())+"s";
     console.log(letItSnow.style.left);
     cover[0].appendChild(letItSnow);
